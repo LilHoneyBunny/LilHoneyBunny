e4929d2ee04c84fb75a192b009c2826c466b7745
LilHoneyBunny <128755776+LilHoneyBunny@users.noreply.github.com>
2023-04-27 10:06:53 -0600

    Iniciando repositorio

---
+++ Convertidor/IServicioClima.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertidor
+{
+    [ServiceContract]
+    public interface IServicioClima
+    {
+        [OperationContract]
+        double convertirCentigradosAFahrenheit(double centigrados);
+        [OperationContract]
+        double convertirFahrenheitACentigrados(double fahrenheit);
+    }
+}
---
+++ Convertidor/IServicioMoneda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Convertidor
+{
+    
+    [ServiceContract]
+    public interface IServicioMoneda
+    {
+
+        [OperationContract]
+        double convertirMXNaUSD(double pesos);
+        [OperationContract]
+        double convertirUSDaMXN(double dolares);
+    }    
+}
